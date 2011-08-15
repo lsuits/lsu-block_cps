9462f79757d43fdabcbf909fb26c607a4c129bf2
Philip Cali <philip.cali@gmail.com>
2011-08-15 10:11:01 -0500

    Dynamic enrollment provider builder.

--- lib.php
+++ lib.php
@@ -3,4 +3,69 @@
 defined('MOODLE_INTERNAL') or die();
 
 class enrol_cps_plugin extends enrol_plugin {
+
+    public static function gen_str() {
+        return function ($key, $a=null) {
+            return get_string($key, 'enrol_cps', $a);
+        };
+    }
+
+    public static function _s($key, $a=null) {
+        return get_string($key, 'enrol_cps', $a);
+    }
+
+    public static function plugin_base() {
+        return self::cps_base('plugins');
+    }
+
+    public static function cps_base($dir='') {
+        return dirname(__FILE__) . (empty($dir) ? '' : '/'.$dir);
+    }
+
+    public static function list_plugins() {
+
+        $base = self::plugin_base();
+
+        $all_files_folders = scandir($base);
+
+        $plugins = array_filter($all_files_folders, function ($file) use ($base) {
+            return is_dir($base . '/' . $file) and !preg_match('/^\./', $file);
+        });
+
+        if (empty($plugins)) {
+            return array();
+        }
+
+        $provide_append = function ($name) {
+            return enrol_cps_plugin::_s("{$name}_name");
+        };
+
+        return array_combine($plugins, array_map($provide_append, $plugins));
+    }
+
+    public static function create_provider() {
+        $provider_name = get_config('enrol_cps', 'enrollment_provider');
+
+        if (!$provider_name) {
+            return false;
+        }
+
+        $class_file = self::plugin_base() . '/' . $provider_name . '/provider.php';
+
+        if (!file_exists($class_file)) {
+            return false;
+        }
+
+        // Require library code
+        $lib_base = self::cps_base('classes');
+        require_once $lib_base . '/provider.php';
+        require_once $lib_base . '/processors.php';
+
+        // Require client code
+        require_once $class_file;
+
+        $provider_class = "{$provider_name}_enrollment_provider";
+
+        return new $provider_class();
+    }
 }
