d720a6f5ee28385da5db007b6c1c182fcc658a30
Philip Cali <philip.cali@gmail.com>
2011-08-18 13:36:16 -0500

    Pre-enrollment process now defined.

--- cps_enrollment.class.php
+++ cps_enrollment.class.php
@@ -11,6 +11,12 @@ class cps_enrollment {
     function __construct() {
         try {
             $this->provider = self::create_provider();
+
+            $lib = self::base('classes/dao');
+
+            require_once $lib . '/lib.php';
+            require_once $lib . '/daos.php';
+
         } catch (Exception $e) {
             $a = self::translate_error($e);
 
@@ -19,11 +25,6 @@ class cps_enrollment {
     }
 
     public function full_process() {
-
-        $lib = self::base('classes/dao');
-
-        require_once $lib . '/lib.php';
-        require_once $lib . '/daos.php';
 
         $now = strftime('%Y-%m-%d', time());
 
@@ -45,7 +46,6 @@ class cps_enrollment {
         }
 
         $this->provider->postprocess();
-        return true;
     }
 
     public function process_semesters($semesters) {
@@ -60,11 +60,7 @@ class cps_enrollment {
                     'session_key' => $semester->session_key
                 );
 
-                $cps = cps_semester::upgrade($semester);
-
-                if ($prev = cps_semester::get($params)) {
-                    $cps->id = $prev->id;
-                }
+                $cps = cps_semester::upgrade_and_get($semester, $params);
 
                 $cps->save();
 
@@ -87,37 +83,29 @@ class cps_enrollment {
                     'cou_number' => $course->number
                 );
 
-                $sections = $courses->sections;
-
-                unset($courses->sections);
-
-                $cps_course = cps_course::upgrade($course);
-
-                if ($prev = cps_course::get($params, true)) {
-                    $cps_course->id = $prev->id;
-                }
+                $cps_course = cps_course::upgrade_and_get($course, $params);
 
                 $cps_course->save();
 
-                $cps_course->sections = array();
-
-                foreach ($sections as $section) {
+                $processed_sections = array();
+                foreach ($cps_course->sections as $section) {
                     $params = array(
                         'courseid' => $cps_course->id,
                         'semesterid' => $semester->id,
                         'sec_number' => $section->sec_number
                     );
 
-                    $cps_section = cps_section::upgrade($section);
+                    $cps_section = cps_section::upgrade($section, $params);
 
-                    if ($prev = cps_section::get($params, true)) {
-                        $cps_section->id = $prev->id;
-                    }
+                    $cps_section->status = 'processed';
 
                     $cps_section->save();
 
-                    $cps_course->sections[] = $cps_section;
+                    $processed_sections[] = $cps_section;
                 }
+
+                // Mutating sections tied to course
+                $cps_course->sections = $processed_sections;
 
                 $processed[] = $cps_course;
             } catch (Exception $e) {
@@ -134,18 +122,91 @@ class cps_enrollment {
     public function process_course_enrollment($semester, $course) {
         $teacher_source = $this->provider->teacher_source();
 
-        $teachers = $teacher_source->teachers($semester, $course);
-
-        foreach ($teachers as $teacher) {
-            // Insert or update teacher users here
-        }
-
         $student_source = $this->provider->student_source();
 
-        $students = $student_source->students($semester, $course);
+        foreach ($course->sections as $section) {
+            $teachers = $teacher_source->teachers($semester, $course, $section);
 
-        foreach ($students as $student) {
-            // Insert or update student users here
+            $students = $student_source->students($semester, $course, $section);
+
+            try {
+                $this->process_teachers($teachers);
+
+                $this->process_students($students);
+            } catch (Exception $e) {
+                $this->errors[] = $e->getMessage();
+            }
+        }
+    }
+
+    public function process_teachers($section, $users) {
+        return $this->fill_role('teacher', $section, $users, function($user) {
+            return array('primary_flag' => $user->primary_flag);
+        });
+    }
+
+    public function process_students($section, $users) {
+        return $this->fill_role('student', $section, $users);
+    }
+
+    public function create_user($u) {
+        $by_idnumber = array('idnumber' => $u->idnumber);
+
+        $by_username = array('username' => $u->username);
+
+        $exact_params = $by_idnumber + $by_username;
+
+        $user = cps_user::upgrade($u);
+
+        if ($prev = cps_user::get($exact_params, true)) {
+            $user->id = $prev->id;
+        } else if ($prev = cps_user::get($by_idnumber, true)) {
+            $user->id = $prev->id;
+        } else if ($prev = cps_user::get($by_username, true)) {
+            $user->id = $prev->id;
+        } else {
+            $user->email = $user->username . $this->settting('user_email');
+            $user->confirmed = $this->settting('user_confirm');
+            $user->city = $this->setting('user_city');
+            $user->country = $this->setting('user_country');
+            $user->firstaccess = time();
+        }
+
+        $user->save();
+
+        return $user;
+    }
+
+    public function setting($key) {
+        return get_config('enrol_cps', $key);
+    }
+
+    private function fill_role($type, $section, $users, $extra_params = null) {
+        $class = 'cps_'.$type;
+
+        foreach ($users as $user) {
+            $cps_user = $this->create_user($user);
+
+            $params = array(
+                'sectionid' => $section->id,
+                'userid' => $cps_user->id
+            );
+
+            if ($extra_params) {
+                $params += $extra_params($cps_user);
+            }
+
+            $cps_type = $class::upgrade($cps_user);
+
+            unset($cps_type->id);
+
+            if ($prev = $class::get($params, true)) {
+                $cps_type->id = $prev->id;
+            }
+
+            $cps_type->status = 'enroll';
+
+            $cps_type->save();
         }
     }
 
