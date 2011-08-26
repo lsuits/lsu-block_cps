45f11c80663df53a01ed5258186ddbab2a3adea6
Philip Cali <philip.cali@gmail.com>
2011-08-26 15:26:48 -0500

    Block link generation

--- block/block_cps.php
+++ block/block_cps.php
@@ -14,13 +14,28 @@ class block_cps extends block_list {
             return $this->content;
         }
 
+        global $CFG;
+
+        require_once $CFG->dirroot . '/enrol/cps/publiclib.php';
+        require_once $CFG->dirroot . '/blocks/cps/classes/lib.php';
+
+        if (!cps_user::is_teacher()) {
+            return $this->content;
+        }
+
         $content = new stdClass;
 
         $content->items = array();
         $content->icons = array();
         $content->footer = '';
 
-        $content->items[] = 'Testing';
+        $preferences = cps_preferences::settings();
+
+        foreach ($preferences as $setting => $name) {
+            $url = new moodle_url("/blocks/cps/$setting.php");
+
+            $content->items[] = html_writer::link($url, $name);
+        }
 
         $this->content = $content;
         return $content;
