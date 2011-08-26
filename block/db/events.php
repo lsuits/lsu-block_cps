a80499698b48c930e2002e0ebc1db8a557d4a917
Philip Cali <philip.cali@gmail.com>
2011-08-26 10:45:54 -0500

    More events to handle.

--- block/db/events.php
+++ block/db/events.php
@@ -2,18 +2,21 @@
 
 $events = array(
     'cps_primary_change',
+    'cps_student_process',
+    'cps_teacher_process',
     'cps_section_process',
     'cps_course_create',
     'cps_course_severed',
     'cps_group_emptied',
     'cps_student_enroll',
-    'cps_student_unenroll'
+    'cps_student_unenroll',
+    'user_updated'
 );
 
 $mapper = function ($event) {
     return array(
         'handlerfile' => '/blocks/cps/eventslib.php',
-        'handlerfunction' => $event . '_handler',
+        'handlerfunction' => array('cps_event_handler', $event),
         'schedule' => 'instant'
     );
 };
