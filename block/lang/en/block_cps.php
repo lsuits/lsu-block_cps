f2b0be8b397dd420ea2d46f8af496f2703f536f0
Philip Cali <philip.cali@gmail.com>
2011-08-26 10:46:38 -0500

    Adding strings for settings

--- block/lang/en/block_cps.php
+++ block/lang/en/block_cps.php
@@ -1,3 +1,27 @@
 <?php
 
 $string['pluginname'] = 'Course Preferences';
+$string['pluginname_desc'] = 'The Course Preference block allows instructors to
+control craetion and enrollment behavior. These are system wide defaults for
+those who do not actually set any data.';
+
+$string['create_days'] = 'Days before Creation';
+$string['create_days_desc'] = 'The number of days before sections are created.';
+
+$string['enroll_days'] = 'Days before Enrollment';
+$string['enroll_days_desc'] = 'The number of days before **created** sections are enrolled.';
+
+$string['course_severed'] = 'Delete upon Severage';
+$string['course_severed_desc'] = 'A course is severed if the Moodle course will
+not longer be handled by the enrollment module, or if enrollment equals zero.';
+
+$string['hide_settings'] = 'CPS Settings';
+
+// Setting names
+$string['creation'] = 'Creation / Enrollment';
+
+$string['unwant'] = 'Unwanted';
+$string['material'] = 'Materials Course';
+$string['split'] = 'Splliting';
+$string['crosslist'] = 'Cross-listing';
+$string['team_request'] = 'Team Teach Requests';
