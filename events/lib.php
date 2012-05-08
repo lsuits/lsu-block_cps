40cdf4391a74905ff00e1fa45c064403bab09670
Philip Cali <philip.cali@gmail.com>
2012-05-08 12:37:36 -0500

    Degree candidates set as 'Y' or 'N' already

--- events/lib.php
+++ events/lib.php
@@ -8,7 +8,6 @@ class cps_meta_ui_element extends meta_data_text_box {
             case 'username':
                 $url = new moodle_url('/user/profile.php', array('id' => $user->id));
                 return html_writer::link($url, $user->username);
-            case 'user_degree':
             case 'user_ferpa': return $this->format_bool($user);
             case 'user_reg_status': return $this->format_date($user);
             default: return parent::format($user);
